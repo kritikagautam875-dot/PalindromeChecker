21f4f973ae5c678feb78b33dd4f586872081ea8a
kritikagautam875-dot <kritikagautam875@gmail.com>
2026-03-03 16:29:21 +0530

    Kritika added UC5

--- palindromechecker/src/PalindromeChecker.java
+++ palindromechecker/src/PalindromeChecker.java
@@ -1,32 +1,20 @@
+import java.util.Stack;
 public class PalindromeChecker {
-        public static boolean isPalindrome(String str) {
-            // Convert string to character array
-            char[] charArray = str.toCharArray();
-
-            // Initialize two pointers
-            int left = 0;
-            int right = charArray.length - 1;
-
-            // Use two-pointer approach to compare characters
-            while (left < right) {
-                // Compare characters at the current pointers
-                if (charArray[left] != charArray[right]) {
-                    return false; // Characters do not match, not a palindrome
-                }
-
-                // Move pointers towards each other
-                left++;
-                right--;
+        public static boolean isPalindrome(String input) {
+            Stack<Character> stack = new Stack<>();
+            for (char c : input.toCharArray()) {
+                stack.push(c);
             }
 
-            return true; // All characters matched, it is a palindrome
+            StringBuilder reversed = new StringBuilder();
+            while (!stack.isEmpty()) {
+                reversed.append(stack.pop());
+            }
+            return input.equals(reversed.toString());
         }
 
         public static void main(String[] args) {
-            String word1 = "madam";
-            String word2 = "hello";
-
-            System.out.println(word1 + " is a palindrome: " + isPalindrome(word1));
-            System.out.println(word2 + " is a palindrome: " + isPalindrome(word2));
+            String text = "radar";
+            System.out.println("Is '" + text + "' a palindrome? " + isPalindrome(text));
         }
     }
